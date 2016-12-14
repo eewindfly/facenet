181b8b1350d17e144ac02ac24f26edf7652693ed
Zack_Hsiao <zackhsiao@emotibot.com>
2016-12-14 18:05:25 +0800

    default to use one gpu

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker rm -f facenet
-NV_GPU=0,1 nvidia-docker run -it -p 8888:8888 -p 6006:6006 --name facenet \
+NV_GPU=0 nvidia-docker run -it -p 8888:8888 -p 6006:6006 --name facenet \
   -v $(pwd):/root/workspace/facenet \
   -v /media/shared_data:/root/datasets \
   eewindfly/facenet
