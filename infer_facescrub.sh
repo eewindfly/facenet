9abdfcd81b1bfe202ce24ea060d257443155eaa9
Zack_Hsiao <zackhsiao@emotibot.com>
2017-02-20 16:49:09 +0800

    update default infer setting

--- infer_facescrub.sh
+++ infer_facescrub.sh
@@ -1,9 +1,9 @@
 cd src
 python infer.py \
   --file_ext png \
-  --pretrained_model ~/models/facenet/20170131-234652/model-20170131-234652.ckpt-250000 \
+  --pretrained_model ~/models/facenet/20170131-005910/model-20170131-005910.ckpt-80000 \
   --data_dir ~/datasets/zackhsiao/FaceScrub/test_cropped/facescrub_aligned/ \
-  --feature_dir ~/datasets/zackhsiao/FaceScrub/features/InceptionResnetV1wCenterLoss/ \
+  --feature_dir ~/datasets/zackhsiao/FaceScrub/features/InceptionResnetV1_CenterLoss_CASIA/ \
   --image_size 299 \
   --model_def models.inception_resnet_v1 \
   --random_crop \
--- infer_megaface.sh
+++ infer_megaface.sh
@@ -1,9 +1,9 @@
 cd src
 python infer.py \
   --file_ext jpg \
-  --pretrained_model ~/models/facenet/20170131-234652/model-20170131-234652.ckpt-250000 \
+  --pretrained_model ~/models/facenet/20170131-005910/model-20170131-005910.ckpt-80000 \
   --data_dir ~/datasets/zackhsiao/MegaFace/data/daniel/FlickrFinal2/ \
-  --feature_dir ~/datasets/zackhsiao/MegaFace/features/InceptionResnetV1wCenterLoss/ \
+  --feature_dir ~/datasets/zackhsiao/MegaFace/features/InceptionResnetV1_CenterLoss_CASIA/ \
   --image_size 299 \
   --model_def models.inception_resnet_v1 \
   --random_crop \
