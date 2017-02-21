4cb6a386d8b220762c1b201c7b9c47c729964d61
Zack_Hsiao <zackhsiao@emotibot.com>
2017-02-22 00:23:37 +0800

    update feature path

--- infer_facescrub.sh
+++ infer_facescrub.sh
@@ -3,7 +3,7 @@ python infer.py \
   --file_ext png \
   --pretrained_model ~/models/facenet/20170131-234652/model-20170131-234652.ckpt-250000 \
   --data_dir ~/datasets/zackhsiao/FaceScrub/test_cropped/facescrub_aligned/ \
-  --feature_dir ~/datasets/zackhsiao/FaceScrub/features/InceptionResnetV1_CenterLoss_CASIA/ \
+  --feature_dir ~/datasets/zackhsiao/FaceScrub/features/InceptionResnetV1_CenterLoss_MS-Celeb-1M/ \
   --image_size 160 \
   --model_def models.inception_resnet_v1 \
   --random_crop \
--- infer_megaface.sh
+++ infer_megaface.sh
@@ -3,7 +3,7 @@ python infer.py \
   --file_ext jpg \
   --pretrained_model ~/models/facenet/20170131-234652/model-20170131-234652.ckpt-250000 \
   --data_dir ~/datasets/zackhsiao/MegaFace/data/daniel/FlickrFinal2/ \
-  --feature_dir ~/datasets/zackhsiao/MegaFace/features/InceptionResnetV1_CenterLoss_CASIA/ \
+  --feature_dir ~/datasets/zackhsiao/MegaFace/features/InceptionResnetV1_CenterLoss_MS-Celeb-1M/ \
   --image_size 160 \
   --model_def models.inception_resnet_v1 \
   --random_crop \
