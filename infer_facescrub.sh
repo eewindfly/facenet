fd194d6b997e9fab8ede903e69e147aa84568845
Zack_Hsiao <zackhsiao@emotibot.com>
2017-02-22 00:21:36 +0800

    update pretrained model

--- infer_facescrub.sh
+++ infer_facescrub.sh
@@ -1,7 +1,7 @@
 cd src
 python infer.py \
   --file_ext png \
-  --pretrained_model ~/models/facenet/20170131-005910/model-20170131-005910.ckpt-80000 \
+  --pretrained_model ~/models/facenet/20170131-234652/model-20170131-234652.ckpt-250000 \
   --data_dir ~/datasets/zackhsiao/FaceScrub/test_cropped/facescrub_aligned/ \
   --feature_dir ~/datasets/zackhsiao/FaceScrub/features/InceptionResnetV1_CenterLoss_CASIA/ \
   --image_size 160 \
--- infer_megaface.sh
+++ infer_megaface.sh
@@ -1,7 +1,7 @@
 cd src
 python infer.py \
   --file_ext jpg \
-  --pretrained_model ~/models/facenet/20170131-005910/model-20170131-005910.ckpt-80000 \
+  --pretrained_model ~/models/facenet/20170131-234652/model-20170131-234652.ckpt-250000 \
   --data_dir ~/datasets/zackhsiao/MegaFace/data/daniel/FlickrFinal2/ \
   --feature_dir ~/datasets/zackhsiao/MegaFace/features/InceptionResnetV1_CenterLoss_CASIA/ \
   --image_size 160 \
